9767b4e2ff6e2052ee5a5ad421280e31253a7932
mptech <mgoembel1@gmail.com>
2025-01-26 02:57:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function createMatrixEffect() {
         ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
         ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-        ctx.fillStyle = "#00BFFF"; // Sky Blue
+        ctx.fillStyle = "#00c3ff"; // Sky Blue
         ctx.font = fontSize + "px monospace";
 
         for (let i = 0; i < drops.length; i++) {
